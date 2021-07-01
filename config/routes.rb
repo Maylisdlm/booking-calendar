8dbe1f36d9d592c3c82d5a5def34842725459aa4
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-07-01 20:20:18 +0200

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'calendars/show'
+  root to: 'calendars#show'
   resources :guides, :calendars
 end
